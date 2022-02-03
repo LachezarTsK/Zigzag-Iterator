091e35cf3b903f96c951cc04510cb82eef4eca82
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-02-03 07:45:10 +0100

    Create ZigzagIterator.java

---
+++ ZigzagIterator.java
@@ -0,0 +1,47 @@
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ZigzagIterator {
+
+    List<List<Integer>> vectors;
+    int[] indexes;
+    int indexVectors;
+    int endedVectors;
+
+    public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
+        vectors = new ArrayList<>();
+        vectors.add(v1);
+        vectors.add(v2);
+        indexes = new int[vectors.size()];
+    }
+
+    public int next() {
+        if (!hasNext()) {
+            throw new IndexOutOfBoundsException(indexes[indexVectors] + " of vector " + indexVectors + " is out of bounds!");
+        }
+
+        int next = vectors.get(indexVectors).get(indexes[indexVectors]);
+        indexes[indexVectors]++;
+        getNextIndexAmongLists();
+
+        return next;
+    }
+
+    public boolean hasNext() {
+        moveForward();
+        return endedVectors < vectors.size();
+    }
+
+    public void moveForward() {
+        endedVectors = 0;
+        while (indexes[indexVectors] == vectors.get(indexVectors).size() && endedVectors < vectors.size()) {
+            endedVectors++;
+            getNextIndexAmongLists();
+        }
+    }
+
+    public void getNextIndexAmongLists() {
+        indexVectors = (indexVectors + 1) % vectors.size();
+    }
+}
